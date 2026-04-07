3e61ff05ce8a00d519a79f6333475167c3f3aaaf
hasan2445 <hasanmohammad2445@gmail.com>
2026-04-07 18:07:31 +0530

    Time: 81 ms (91.24%) | Memory: 105.8 MB (82.36%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int,int>>> adj(n);
+        
+        for(auto &e : connections) {
+            int u = e[0], v = e[1];
+            adj[u].push_back({v, 1}); // wrong direction
+            adj[v].push_back({u, 0}); // correct
+        }
+        
+        vector<int> vis(n, 0);
+        queue<int> q;
+        q.push(0);
+        vis[0] = 1;
+        
+        int ans = 0;
+        
+        while(!q.empty()) {
+            int node = q.front(); q.pop();
+            
+            for(auto &it : adj[node]) {
+                int nei = it.first;
+                int cost = it.second;
+                
+                if(!vis[nei]) {
+                    ans += cost;
+                    vis[nei] = 1;
+                    q.push(nei);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
