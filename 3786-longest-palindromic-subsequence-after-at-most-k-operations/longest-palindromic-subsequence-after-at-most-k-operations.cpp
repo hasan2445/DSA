3d307b24c961916dd0514427ed1c85273d53c33b
hasan2445 <hasanmohammad2445@gmail.com>
2026-06-08 17:13:31 +0530

    Time: 389 ms (82.84%) | Memory: 247.3 MB (6.72%) - LeetSync

--- 3786-longest-palindromic-subsequence-after-at-most-k-operations/longest-palindromic-subsequence-after-at-most-k-operations.cpp
+++ 3786-longest-palindromic-subsequence-after-at-most-k-operations/longest-palindromic-subsequence-after-at-most-k-operations.cpp
@@ -1,97 +1,44 @@
 class Solution {
 public:
     int longestPalindromicSubsequence(string s, int k) {
-
-        s.insert(s.begin(), '0');
-
         int n = s.size();
-
-        vector<vector<vector<int>>> dp(
-            n,
-            vector<vector<int>>(n, vector<int>(k + 1, 0))
-        );
-
-        for (int len = 1; len <= 2; len++) {
-
-            for (int i = 1; i + len - 1 < n; i++) {
-
+        s.insert(s.begin(), '0');
+        int ans = 1;
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(n + 1, vector<int>(k + 1, 0)));
+        
+        for(int i = 1; i <= n; i++) {
+            for(int x = 0; x <= k; x++) {
+                dp[i][i][x] = 1;
+            }
+        }
+        
+        for(int len = 2; len <= n; len++) {
+            for(int i = 1; i + len - 1 <= n; i++) {
                 int j = i + len - 1;
-
-                for (int op = 0; op <= k; op++) {
-
-                    if (len == 1) {
-
-                        dp[i][j][op] = 1;
-                    }
-                    else {
-
-                        dp[i][j][op] = max(
-                            dp[i + 1][j][op],
-                            dp[i][j - 1][op]
-                        );
-
-                        if (s[i] == s[j]) {
-
-                            dp[i][j][op] = max(
-                                dp[i][j][op],
-                                2
-                            );
+                int cost = min(abs(s[i] - s[j]), (26 - abs(s[i] - s[j])));
+                
+                for(int x = 0; x <= k; x++) {
+                    dp[i][j][x] = max(dp[i + 1][j][x], dp[i][j - 1][x]);
+                    
+                    if(s[i] == s[j]) {
+                        if(len == 2) {
+                            dp[i][j][x] = max(dp[i][j][x], 2);
+                        } else {
+                            dp[i][j][x] = max(dp[i][j][x], 2 + dp[i + 1][j - 1][x]);
+                            ans = len;
                         }
-                        else {
-
-                            int cost = min(
-                                abs(s[i] - s[j]),
-                                26 - abs(s[i] - s[j])
-                            );
-
-                            if (op >= cost) {
-
-                                dp[i][j][op] = max(
-                                    dp[i][j][op],
-                                    2
-                                );
+                    } else {
+                        if(x >= cost) {
+                            if(len == 2) {
+                                dp[i][j][x] = max(2, dp[i][j][x]); 
+                            } else {
+                                dp[i][j][x] = max(dp[i][j][x], dp[i + 1][j - 1][x - cost] + 2);
                             }
                         }
                     }
                 }
             }
         }
-
-        for (int len = 3; len < n; len++) {
-
-            for (int i = 1; i + len - 1 < n; i++) {
-
-                int j = i + len - 1;
-
-                for (int op = 0; op <= k; op++) {
-
-                   
-
-                    if (s[i] == s[j]) {
-
-                        dp[i][j][op] = 2 + dp[i + 1][j - 1][op];
-                    }
-                    else {
-
-                        int cost = min(
-                            abs(s[i] - s[j]),
-                            26 - abs(s[i] - s[j])
-                        );
-                         dp[i][j][op] = max(
-                        dp[i + 1][j][op],
-                        dp[i][j - 1][op]
-                    );
-
-                        if (op >= cost) {
-
-                            dp[i][j][op]=max(dp[i][j][op],2 + dp[i + 1][j - 1][op - cost]);
-                            
-                        }
-                    }
-                }
-            }
-        }
-
-        return dp[1][n - 1][k];
+        return dp[1][n][k];
     }
 };
