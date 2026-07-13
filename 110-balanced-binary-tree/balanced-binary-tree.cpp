923982648e0ea4b643fba35ac992d9918bc7730f
hasan2445 <hasanmohammad2445@gmail.com>
2026-07-13 23:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (83.90%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -10,22 +10,16 @@
  * };
  */
 class Solution {
+
 public:
-   
-        int height(TreeNode* root) {
-    if (!root) return 0;
-    return 1 + max(height(root->left), height(root->right));
-}
-
-bool isBalanced(TreeNode* root) {
-    if (!root) return true;
-    
-    int left = height(root->left);
-    int right = height(root->right);
-    
-    if (abs(left - right) > 1) return false;
-    
-    return isBalanced(root->left) && isBalanced(root->right);
-
+    int height(TreeNode* root)
+    {
+        if(root==NULL) return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return true;
+        if(abs(height(root->left)-height(root->right))>1) return false;
+        return isBalanced(root->right) && isBalanced(root->left);
     }
 };
